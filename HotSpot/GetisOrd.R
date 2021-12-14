ad4a9226e9ae3825eaddf0d4cbb931ca85eee364
luxn <luxn@users.noreply.github.com>
2021-12-14 14:23:46 +0100

    Update GetisOrd.R

--- HotSpot/GetisOrd.R
+++ HotSpot/GetisOrd.R
@@ -1,10 +1,10 @@
 wave <- read_csv("./wave.weekly.csv")
-kw <- 45
+kw <- 16
 wave <- wave %>% filter(Kalenderwoche == kw) %>% append_landkreis_geometry()
 
 nb <- dnearneigh(st_centroid(wave), 0, 60000) # 60km
 
-nb_lw <- nb2listw(nb, style = 'B')
+nb_lw <- nb2listw(nb, style = 'W')
 
 local_g <- localG(wave$Inzidenz, nb_lw)
 local_g <- cbind(wave, as.matrix(local_g))
@@ -15,5 +15,5 @@ pal <- c("#0000FF",  "#6666FF", "#9999FF", "#FFFFFF", "#FF6666", "#FF3333", "#FF
 
 tm_shape(local_g) + tm_fill("gstat",
                             palette = pal,
-                            style = "quantile", midpoint = NA) + tm_borders(alpha=.4)
+                            style = "pretty") + tm_borders(alpha=.4)
 
