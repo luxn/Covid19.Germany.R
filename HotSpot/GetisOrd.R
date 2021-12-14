b4786c4c0926ad8064b612fdf748ad84ccd35307
luxn <luxn@users.noreply.github.com>
2021-12-14 15:14:55 +0100

    added hotspot script into function

--- HotSpot/GetisOrd.R
+++ HotSpot/GetisOrd.R
@@ -1,4 +1,28 @@
-wave <- read_csv("./wave.weekly.csv")
+hotspot.gstat <- function(df, wavetype, wavefilter) {
+  if (wavetype == "weekly") {
+    wave <- df %>% filter(Kalenderwoche == wavefilter) # KW: 43
+  } else if (wavetype == "daily") {
+    wave <- df %>% filter(Meldedatum == wavefilter) # Datum: '2021-10-22'
+  } else {
+    wave <- df
+  }
+  wave <- wave %>% append_landkreis_geometry
+
+  nb <- dnearneigh(st_centroid(wave), 0, 60000) # 60km
+  nb_lw <- nb2listw(nb, style = 'W')
+  local_g <- localG(wave$Inzidenz, nb_lw)
+  local_g <- cbind(wave, as.matrix(local_g))
+  local_g <- rename(local_g, gstat = as.matrix.local_g.)
+
+  pal <-  c("blue",rgb(0,0,1,alpha=0.4),"white", rgb(1,0,0,alpha=0.4),"red")
+
+  tm_shape(local_g) + tm_fill("gstat", n=5,
+                              palette = pal,
+                              style = "fixed", breaks = c(-Inf, -2, -1, 1, 2, +Inf)) + tm_borders(alpha=.4)
+}
+
+
+wv <- read_csv("./wave.weekly.csv")
 kw <- 16
 wave <- wave %>% filter(Kalenderwoche == kw) %>% append_landkreis_geometry()
 
@@ -8,12 +32,11 @@ nb_lw <- nb2listw(nb, style = 'W')
 
 local_g <- localG(wave$Inzidenz, nb_lw)
 local_g <- cbind(wave, as.matrix(local_g))
+local_g <- mutate(local_g, gstat = as.matrix(local_g))
 
-names(local_g)[8] <- "gstat"
+pal <-  c("blue",rgb(0,0,1,alpha=0.4),"white", rgb(1,0,0,alpha=0.4),"red")
 
-pal <- c("#0000FF",  "#6666FF", "#9999FF", "#FFFFFF", "#FF6666", "#FF3333", "#FF0000")
-
-tm_shape(local_g) + tm_fill("gstat",
+tm_shape(local_g) + tm_fill("gstat", n=5,
                             palette = pal,
-                            style = "pretty") + tm_borders(alpha=.4)
+                            style = "fixed", breaks = c(-Inf, -2, -1, 1, 2, +Inf)) + tm_borders(alpha=.4)
 
--- HotSpot/LISA.R
+++ HotSpot/LISA.R
@@ -1,4 +1,51 @@
-wave <- read_csv("./wave.weekly.csv")
+hotspot.lisa <- function(df, wavetype, wavefilter) {
+  if (wavetype == "weekly") {
+    wave <- df %>% filter(Kalenderwoche == wavefilter) # KW: '43'
+  } else if (wavetype == "daily") {
+    wave <- df %>% filter(Meldedatum == wavefilter) # Datum: '2021-10-22'
+  } else {
+    wave <- df
+  }
+  wave <- wave %>% append_landkreis_geometry
+
+  neighbours <- poly2nb(wave, queen = TRUE)
+  local <- localmoran(x = wave$Inzidenz, listw = nb2listw(neighbours, style = "W"))
+  quadrant <- vector(mode="numeric",length=nrow(local))
+
+  # centers the variable of interest around its mean
+  m.inzidenz <- wave$Inzidenz - mean(wave$Inzidenz)
+
+  # centers the local Moran's around the mean
+  m.local <- local[,1] - mean(local[,1])
+
+  # significance threshold
+  signif <- 0.1
+
+  # builds a data quadrant
+  quadrant[m.inzidenz >0 & m.local>0] <- 4
+  quadrant[m.inzidenz <0 & m.local<0] <- 1
+  quadrant[m.inzidenz <0 & m.local>0] <- 2
+  quadrant[m.inzidenz >0 & m.local<0] <- 3
+  quadrant[local[,5]>signif] <- 0
+
+  # plot in r
+  brks <- c(0,1,2,3,4)
+  colors <- c("white","blue",rgb(0,0,1,alpha=0.4),rgb(1,0,0,alpha=0.4),"red")
+
+  plot(as_Spatial(wave) ,border="lightgray",col=colors[findInterval(quadrant,brks,all.inside=FALSE)])
+  title(sprintf("hotspot.lisa: %s %s", wavetype, wavefilter))
+  box()
+  legend("bottomleft", legend = c("insignificant","low-low","low-high","high-low","high-high"),fill=colors,bty="n")
+
+}
+
+
+
+
+
+
+
+htwave <- read_csv("./wave.weekly.csv")
 kw <- 35
 
 wave <- wave %>% filter(Kalenderwoche == kw) %>% append_landkreis_geometry
