9931948a0469e40a28650d42097f1548d6e19d9b
luxn <luxn@users.noreply.github.com>
2022-01-16 15:34:01 +0100

    added LISA pvalues Plot/Map

--- SpatialAnalysis/LISALocalMoranPlot.R
+++ SpatialAnalysis/LISALocalMoranPlot.R
@@ -17,12 +17,19 @@ for (dframe in dfs) {
   lisa_colors <- lisa_colors(lisa)
   lisa_labels <- lisa_labels(lisa)
   lisa_clusters <- lisa_clusters(lisa)
+  lisa_pvalues <- lisa_pvalues(lisa)
 
   p <- ggplot(df) +
     geom_sf(aes(fill=factor(lisa_clusters)), lwd =0.25) +
     scale_fill_manual(values = lisa_colors, name= "LISA Cluster", labels=lisa_labels) +
-    ggtitle(sprintf("Local Moran LISA Cluster - Phase %s", i))
+    ggtitle(sprintf("Local Moran LISA Cluster - Phase %s | p <0.05", i))
 
-  ggsave(sprintf("./Images/lisa.localmoran.phase%s.png", i), p, dpi="retina", width=7.5, height=6)
+  p2 <- ggplot(df) +
+    geom_sf(aes(fill=cut(lisa_pvalues, c(0, 0.001, 0.01, 0.05, 1), labels=c("less than 0.001", "less than 0.01", "less than 0.05", "not significant"))), lwd =0.25) +
+    scale_fill_manual(values = c('#006d2c', '#31a354', '#bae4b3', "grey"), name= "LISA pvalues",) +
+    ggtitle(sprintf("Local Moran LISA pvalues - Phase %s", i))
+
+  ggsave(sprintf("./Images/lisa.localmoran.phase%s.cluster.png", i), p, dpi="retina", width=7.5, height=6)
+  ggsave(sprintf("./Images/lisa.localmoran.phase%s.pvalues.png", i), p2, dpi="retina", width=7.5, height=6)
   i <- i + 1
 }
